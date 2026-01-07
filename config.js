58facd30b7bb64acf858e104f8b902959efb760f
zahid53402 <zahid.ali.00347@gmail.com>
2026-01-07 20:42:19 +0500

    Set default SESSION_ID in config.js
    Updated SESSION_ID to a default value for testing.

--- config.js
+++ config.js
@@ -5,7 +5,7 @@ function convertToBool(text, fault = 'true') {
     return text === fault ? true : false;
 }
 module.exports = {
-SESSION_ID: process.env.SESSION_ID || "",
+SESSION_ID: process.env.SESSION_ID || "QADEER-AI~KEdixbYA#A29ei3iwLulHCGVqxTb7cu5_R0-keKcwr681JUH5hnI",
 // add your Session Id 
 AUTO_STATUS_SEEN: process.env.AUTO_STATUS_SEEN || "true",
 // make true or false status auto seen
